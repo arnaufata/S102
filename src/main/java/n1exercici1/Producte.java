f63ca3081b9eff1432c6b5a8c26ed3519d610caa
Arnau <arnalvberita@gmail.com>
2024-05-28 13:42:56 +0200

    Construcció classe Producte

---
+++ src/main/java/n1exercici1/Producte.java
@@ -0,0 +1,26 @@
+package n1exercici1;
+
+public class Producte {
+
+    // ATRIBUTS
+
+    private String nom;
+    private double preu;
+
+    // CONSTRUCTOR
+
+    public Producte (String nom, double preu){
+        this.nom = nom;
+        this.preu = preu;
+    }
+
+    // GETTERS & SETTERS
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+}
--- src/main/java/n1exercici1/S102n1exercici1.java
+++ src/main/java/n1exercici1/S102n1exercici1.java
@@ -1,4 +1,8 @@
 package n1exercici1;
 
 public class S102n1exercici1 {
+
+    public static void main(String[] args) {
+
+    }
 }
