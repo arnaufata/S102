2615383cdad851812e2eb6c22bc7445889a7dfda
Arnau <arnalvberita@gmail.com>
2024-05-28 12:52:19 +0200

    Pujant exercici

---
+++ src/main/java/n2exercici1/S102n2exercici1.java
@@ -0,0 +1,22 @@
+package n2exercici1;
+
+public class S102n2exercici1 {
+
+	public static void main(String[] args) {
+		
+		// Mètodes a implantar capturant l’excepció de la classe InputMismatchException:
+		
+	    Entrada.llegirByte("Introdueix la teva edat: ");
+		Entrada.llegirInt("Introdueix l'any en que ens trobem: ");
+		Entrada.llegirFloat("Introdueix la teva alçada: ");
+		Entrada.llegirDouble("Introdueix el teu pes: "); 
+		
+		// Mètodes a implantar capturant l’excepció de la classe Exception:
+		
+		Entrada.llegirChar("Introdueix la lletra del teu DNI: ");
+		Entrada.llegirString("Introdueixi el seu nom: ");
+		Entrada.llegirSiNo("Vols continuar? (s/n): ");
+
+	}
+
+}
